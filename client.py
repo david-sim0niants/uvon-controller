60706da2f7d4f0a481dae3f305a48e61200afa8d
David <david.simonyants@gmail.com>
2021-05-26 22:20:22 +0400

    server/client scripts fixed a lot

--- client.py
+++ client.py
@@ -5,13 +5,27 @@ import sys
 import cv2
 
 
+def get_local_address():
+    s = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    address = s.getsockname()
+    s.close()
+    return address
+
+
+local_address = get_local_address()
+
 client = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
-client.connect((sys.args[1], int(sys.args[2])))
+client.bind(('127.0.0.1', local_address[1]))
 
 while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
+
+    client_ip_bytes = bytes(int(x) for x in local_address[0].split('.'))
+    client_port_bytes = local_address[1].to_bytes(2, 'big')
+    client.sendto(client_ip_bytes + client_port_bytes, (sys.argv[1], int(sys.argv[2])))
 
     header = client.recv(4)
     bytes_len = int.from_bytes(header)
--- server.py
+++ server.py
@@ -7,19 +7,19 @@ from csi_camera import *
 server = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
 server.bind(('127.0.0.1', 1234))
 server.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, 10)
-server.listen(5)
-
-
-client, client_address = server.accept()
 
 csi = start_capturing()
 
 while True:
+    client_data = server.recvfrom(6)
+    client_ip = '%d.%d.%d.%d' % (client_data[0], client_data[1], client_data[2], client_data[3])
+    client_port = '%d' % int.from_bytes(client_data[4:])
+
     res, img = next(csi)
     if not res:
         continue
     buffer = io.BytesIO() 
     np.save(buffer, img)
     img_bytes = buffer.getvalue()
-    client.send(len(img_bytes).to_bytes(4, byteorder='big', signed=True) + img_bytes)
+    server.sendto(len(img_bytes).to_bytes(4, byteorder='big', signed=True) + img_bytes, (client_ip, client_port))
 
