52e10610f2b4d05d299a0eefc079c4a9cb6b133d
David <david.simonyants@gmail.com>
2021-05-28 02:58:09 +0400

    ROBOT CORRECTLY MOVING git add .

--- controller.py
+++ controller.py
@@ -4,6 +4,8 @@ from breezyslam.sensors import RPLidarA1
 import sys
 import numpy as np
 import atexit
+import serial
+import time
 
 
 
@@ -11,8 +13,6 @@ if __name__ == '__main__':
     
     lidar = rplidar.RPLidar(sys.argv[1])
 
-    atexit.register(lidar.stop)
-    atexit.register(lidar.disconnect)
 
     print('RPLidar health:', lidar.get_health()[0])
     print('RPLidar info: ', lidar.get_info())
@@ -21,6 +21,23 @@ if __name__ == '__main__':
     scans_iterator = lidar.iter_scans()
 
     slam = RMHC_SLAM(RPLidarA1(), 800, 5)
+    
+    
+    uvon_dev = serial.Serial(sys.argv[2], 115200)
+    time.sleep(2)
+    uvon_dev.write(b'2\n')
+    uvon_dev.write(b'I1\n')
+
+    atexit.register(lambda: uvon_dev.write(b'2\n'))
+    atexit.register(lambda: uvon_dev.write(b'I0\n'))
+    atexit.register(lidar.stop)
+    atexit.register(lidar.disconnect)
+
+    forward_cmd = b'm1,15,0,15\n'
+    backward_cmd = b'm0,15,1,15\n'
+    left_cmd = b'm1,1,0,15\n'
+    right_cmd = b'm1,15,0,1\n'
+
 
     while True:
         scans = np.array(next(scans_iterator))
@@ -28,5 +45,18 @@ if __name__ == '__main__':
 
         x, y, theta = slam.getpos()
 
+        theta = np.arcsin(np.sin(theta * np.pi / 180)) * 180 / np.pi
+
         print(x, y, theta)
+
+        if -10 <= theta <= 10:
+            uvon_dev.write(forward_cmd)
+        elif -170 < theta < -10:
+            uvon_dev.write(left_cmd)
+        elif 10 < theta < 170:
+            uvon_dev.write(right_cmd)
+        else:
+            uvon_dev.write(backward_cmd)
+        time.sleep(0.1)
+
 
--- uvon_interactive.py
+++ uvon_interactive.py
@@ -12,7 +12,7 @@ def reading_thread(dev : serial.Serial):
 
 def writing_thread(dev : serial.Serial):
     while True:
-        cmd = input('Enter Cmd: ').encode()
+        cmd = (input('Enter Cmd: ') + '\n').encode()
         uvon_dev.write(cmd)
         time.sleep(0.001)
 
@@ -20,9 +20,12 @@ def writing_thread(dev : serial.Serial):
 if __name__ == '__main__':
 
     port = sys.argv[1]
-    baudrate = int(sys.argv[2]) if len(sys.argv) > 1 else 115200
+    baudrate = int(sys.argv[2]) if len(sys.argv) > 2 else 115200
     
     uvon_dev = serial.Serial(port, baudrate)
+    writing_thread(uvon_dev)
+    
+    exit()
 
     reading = th.Thread(target=reading_thread, args=(uvon_dev,))
     writing = th.Thread(target=writing_thread, args=(uvon_dev,))
